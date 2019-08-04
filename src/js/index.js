15904825f7185b72100cf6d73178646f8f230c5e
victory studio <hello@victory.studio>
2019-08-04 15:20:22 +0300

    вроде закончил верстку на телефон

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 "use strict";
+var checkbox = document.getElementById('checkbox');
+checkbox.addEventListener( 'change', function() {
+    if(this.checked) {
+        // document.body.classList.add('js__no_scroll-body');
+        document.getElementById('opened').classList.add('opened');
+        document.getElementById('scroll').classList.add('js__no_scroll');
+        // blk()
+    } else {
+        // document.body.classList.remove('js__no_scroll-body');
+        document.getElementById('opened').classList.remove('opened');
+        document.getElementById('scroll').classList.remove('js__no_scroll');
+    }
+});
+
+// function blk() {
+//   document.getElementById('scroll').classList.add('js__no_scroll');
+// }
