24a705063c51116ca7a640bfdbd73ff5fac39913
victory studio <hello@victory.studio>
2019-08-10 23:35:48 +0300

    добавил затухание и плавное появление контента

--- src/js/index.js
+++ src/js/index.js
@@ -5,10 +5,16 @@ checkbox.addEventListener( 'change', function() {
         // document.body.classList.add('js__no_scroll-body');
         document.getElementById('opened').classList.add('opened');
         document.getElementById('scroll').classList.add('js__no_scroll');
+        document.getElementById('opened').classList.remove('header_fading');
         // blk()
     } else {
         // document.body.classList.remove('js__no_scroll-body');
         document.getElementById('opened').classList.remove('opened');
         document.getElementById('scroll').classList.remove('js__no_scroll');
+        document.getElementById('opened').classList.add('header_fading');
     }
 });
+
+
+//
+//
