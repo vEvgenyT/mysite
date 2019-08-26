764000f1e70231ee9aad1c0e09b5bee66bfa1005
victory studio <hello@victory.studio>
2019-08-26 13:30:59 +0300

    мелкие правки перед проверкой

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,22 @@
 var checkbox = document.getElementById('checkbox');
 checkbox.addEventListener( 'change', function() {
     if(this.checked) {
-        // document.body.classList.add('js__no_scroll-body');
         document.getElementById('opened').classList.add('opened');
         document.getElementById('scroll').classList.add('js__no_scroll');
         document.getElementById('opened').classList.remove('header_fading');
-        // blk()
     } else {
-        // document.body.classList.remove('js__no_scroll-body');
         document.getElementById('opened').classList.remove('opened');
         document.getElementById('scroll').classList.remove('js__no_scroll');
         document.getElementById('opened').classList.add('header_fading');
     }
 });
 
-// setTimeout(function(){
-//   var a = document.getElementsByClassName('main_start');
-//   a[0].classList.add('main__title_fonts');
-//   a[0].classList.add('main__text_margin');
-//   a[0].classList.remove('main_start');
-// }, 1900);
+var click = document.getElementById('link');
+var radioButton = document.getElementById('checkbox');
 
-//
-//
+  click.onclick = function() {
+    document.getElementById('opened').classList.remove('opened');
+        document.getElementById('scroll').classList.remove('js__no_scroll');
+        document.getElementById('opened').classList.add('header_fading');
+    radioButton.checked = false;
+  };
